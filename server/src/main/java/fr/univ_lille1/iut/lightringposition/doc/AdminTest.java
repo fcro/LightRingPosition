c0843c8cc374f81e23a33fec735e4f26f51bdb57
Florian CROSNIER <florian.crosnier@etudiant.univ-lille1.fr>
2015-03-11 16:34:30 +0100

    Classe pour les tests manuels et les démos (création de la DB)

---
+++ server/src/main/java/fr/univ_lille1/iut/lightringposition/doc/AdminTest.java
@@ -0,0 +1,15 @@
+package fr.univ_lille1.iut.lightringposition.doc;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import fr.univ_lille1.iut.lightringposition.db.DBUtil;
+
+@Path("admin")
+public class AdminTest {
+	@GET
+	@Path("dbcreate")
+	public void createDB() {
+		DBUtil.getDAO().initDB();
+	}
+}
