86e8a950e65b276a47f127e28f96074dcef8da4e
Florian CROSNIER <florian.crosnier@etudiant.univ-lille1.fr>
2015-03-11 13:11:44 +0100

    Revert commit d2553d66 foireux (utilisation de Case dans Plateau)

--- server/src/main/java/fr/univ_lille1/iut/lightringposition/game/Plateau.java
+++ server/src/main/java/fr/univ_lille1/iut/lightringposition/game/Plateau.java
@@ -4,58 +4,64 @@ import java.util.Random;
 
 public class Plateau {
 	int val;
-	char[][]plateau;
+	Case[][]plateau;
 	int taille;
-	char valeurObstacles;
-	public Plateau(int taille, char valeurObstacles){
+	public Plateau(int taille){
 		this.taille = taille;
-		this.valeurObstacles = valeurObstacles;
-		this.plateau= new char[taille][taille];
+		this.plateau= new Case[taille][taille];
 
 	}
 
 	public void generation(){
-	
+
 		for(int i=0;i<taille; ++i){
 			for( int j=0; j<taille; ++j){
-				plateau[i][j]='.';
-
+				plateau[i][j] = new Case();
 			}
 		}
+
 		Random r= new Random();
 		val = r.nextInt(plateau.length*plateau.length*10/100)+plateau.length*plateau.length*10/100;
-		for(int i=0; i<=val;i++)
-			plateau[r.nextInt(plateau.length)][r.nextInt(plateau.length)]=valeurObstacles;
-
+		for(int i=0; i<=val;i++) {
+			int x = r.nextInt(plateau.length);
+			int y = r.nextInt(plateau.length);
+			plateau[x][y].setEstObstacle(true);
+			plateau[x][y].setEstVide(false);
+		}
 
 
 	}
 	public String toString(){
 
-		    String aString;     
-		    aString = "";
-		    int column;
-		    int row;
+		String aString;
+		aString = "";
+		int column;
+		int row;
 
-		    for (row = 0; row < plateau.length; row++) {
-		        for (column = 0; column < plateau[0].length; column++ ) {
-		        aString = aString + " " + plateau[row][column];
-		        }
-		    aString = aString + "\n";
-		    }
-
-		    return aString;
+		for (row = 0; row < plateau.length; row++) {
+			for (column = 0; column < plateau[0].length; column++ ) {
+				aString = aString + " " + plateau[row][column];
+			}
+			aString = aString + "\n";
 		}
-	
-	public int getVal() {
-		return val;
+
+		return aString;
 	}
 
 
+
+
 	public static void main(String[]args){
-		Plateau p= new Plateau(20,'Y');
+		Plateau p= new Plateau(20);
 		p.generation();
 		System.out.println(p);
-		
+
+	}
+	public void placementJoueur(){
+
+	}
+
+	public int getVal() {
+		return val;
 	}
 }
