eac55d248d85ec3d0b74270543f2cc7897e55040
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-03-13 15:17:48 +0100

    Correction TestPlateau

--- src/test/java/fr/univ_lille1/iut/lightringposition/test/TestPlateau.java
+++ src/test/java/fr/univ_lille1/iut/lightringposition/test/TestPlateau.java
@@ -13,10 +13,10 @@ public class TestPlateau {
 	@Test
 	public void test(){
 		List<Joueur> liste = new ArrayList<Joueur>();
-		for(int i=0;i<liste.size();i++) {
+		for(int i=0;i<10;i++) {
 			liste.add(new Joueur("toto"));
 		}
-		for(int i=0;i<1000000;i++){
+		for(int i=0;i<10000;i++){
 			Plateau p = new Plateau(20,liste);
 			p.generation();
 			assertTrue(p.getVal() <= 80);
