abc575b6d6e8d9f480dc0711a3528ed89d1752f6
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-03-17 14:35:42 +0100

    Revert "envoi de données du tchat au serveur"
    Erreur de manip qui a supprimé les dernières modif sur Account.java

--- src/main/java/fr/univ_lille1/iut/lightringposition/doc/Account.java
+++ src/main/java/fr/univ_lille1/iut/lightringposition/doc/Account.java
@@ -1,17 +1,21 @@
 package fr.univ_lille1.iut.lightringposition.doc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import fr.univ_lille1.iut.lightringposition.db.DBUtil;
 import fr.univ_lille1.iut.lightringposition.struct.LoginPwd;
 import fr.univ_lille1.iut.lightringposition.struct.User;
+import fr.univ_lille1.iut.lightringposition.util.UserSessions;
 import fr.univ_lille1.iut.lightringposition.util.PwdEncrypt;
 
 @Path("account")
@@ -20,8 +24,9 @@ public class Account {
 	@GET
 	@Path("{login}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public User getUserInfo(@PathParam("login") String login) {
+	public User getUserInfo(@Context HttpServletRequest req, @PathParam("login") String login) {
 		User user = DBUtil.getDAO().findUserByLogin(login);
+
 		return user;
 	}
 
@@ -29,14 +34,38 @@ public class Account {
 	@Path("auth")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response authenticate(LoginPwd loginPwd) {
-		String loginInDB = DBUtil.getDAO().getLoginByLoginPassword
-				(loginPwd.getLogin(),PwdEncrypt.encrypt(loginPwd.getPassword()));
+	public Response authenticate(@Context HttpServletRequest req, LoginPwd loginPwd) {
+		User userInDB = DBUtil.getDAO().findUserByLoginPassword
+				(loginPwd.getLogin(), PwdEncrypt.encrypt(loginPwd.getPassword()));
 
-		if (loginInDB != null && loginInDB.equals(loginPwd.getLogin()))
+		if (userInDB != null) {
+			if (!UserSessions.isUserConnected(userInDB) && req != null) {
+				HttpSession session = req.getSession(true);
+				UserSessions.addUser(userInDB, session);
+			}
+
 			return Response.ok("Identifiants valides").build();
+		}
 
 		return Response.status(403).entity("Identifiants invalides").build();
+	}
+
+	@POST
+	@Path("disconnect")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response disconnect(@Context HttpServletRequest req) {
+		if (req != null) {
+			HttpSession session = req.getSession();
+			User user = UserSessions.getUserBySession(req.getSession());
+			if (user != null && session != null &&
+					session.equals(UserSessions.getSessionByUser(user))) {
+				UserSessions.removeUser(user);
+
+				return Response.ok().build();
+			}
+		}
+
+		return Response.status(403).build();
 	}
 
 	@POST
@@ -50,6 +79,7 @@ public class Account {
 
 		DBUtil.getDAO().insertUser(user.getLogin(), PwdEncrypt.encrypt(user.getPassword()),
 				user.getEmail(), user.getNickname(), null);
+
 		return Response.status(201).entity("Utilisateur créé").build();
 	}
 }
