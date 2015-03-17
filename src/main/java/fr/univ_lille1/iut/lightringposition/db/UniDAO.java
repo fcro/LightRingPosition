a8e30bb1d5df10f1f604c6d137d487e56061ef53
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-03-17 14:31:55 +0100

    Déplacement de UserMapper dans un fichier propre

--- src/main/java/fr/univ_lille1/iut/lightringposition/db/UniDAO.java
+++ src/main/java/fr/univ_lille1/iut/lightringposition/db/UniDAO.java
@@ -1,40 +1,14 @@
 package fr.univ_lille1.iut.lightringposition.db;
 
-import java.io.File;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.skife.jdbi.v2.StatementContext;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
-import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
-import org.skife.jdbi.v2.tweak.ResultSetMapper;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 
 import fr.univ_lille1.iut.lightringposition.struct.User;
-import fr.univ_lille1.iut.lightringposition.util.InvalidUserException;
 
+@RegisterMapper(UserMapper.class)
 public interface UniDAO {
-
-	public class UserMapper implements ResultSetMapper<User> {
-		public User map(int index, ResultSet r, StatementContext ctx) throws SQLException {
-			if (r.getString("avatar") != null)
-				try {
-					return new User(r.getString("login"), r.getString("password"), r.getString("email"),
-						r.getString("nickname"), new File(r.getString("avatar")));
-				} catch (InvalidUserException e) {
-					return null;
-				}
-			else
-				try {
-					return new User(r.getString("login"), r.getString("password"), r.getString("email"),
-							r.getString("nickname"));
-				} catch (InvalidUserException e) {
-					return null;
-				}
-		}
-	}
-
 	@SqlUpdate("CREATE TABLE USER(" +
 			"login			VARCHAR(20)			PRIMARY KEY," +
 			"password		VARCHAR(32)			NOT NULL," +
@@ -66,11 +40,9 @@ public interface UniDAO {
 			@Bind("email") String email, @Bind("nickname") String nickname, @Bind("avatar") String avatar);
 
 	@SqlQuery("SELECT * FROM USER WHERE login = :login")
-	@Mapper(UserMapper.class)
 	User findUserByLogin(@Bind("login") String login);
 
 	@SqlQuery("SELECT * FROM USER WHERE login = :login AND password = :password")
-	@Mapper(UserMapper.class)
 	User findUserByLoginPassword(@Bind("login") String login, @Bind("password") String password);
 
 	void close();
---
+++ src/main/java/fr/univ_lille1/iut/lightringposition/db/UserMapper.java
@@ -0,0 +1,35 @@
+package fr.univ_lille1.iut.lightringposition.db;
+
+import java.io.File;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import fr.univ_lille1.iut.lightringposition.struct.User;
+import fr.univ_lille1.iut.lightringposition.util.InvalidUserException;
+
+/**
+ * Permet de récupérer une instance de User lorsqu'on utilise UniDAO
+ * pour trouver un utilisateur
+ *
+ */
+public class UserMapper implements ResultSetMapper<User> {
+	public User map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+		if (r.getString("avatar") != null)
+			try {
+				return new User(r.getString("login"), r.getString("password"), r.getString("email"),
+						r.getString("nickname"), new File(r.getString("avatar")));
+			} catch (InvalidUserException e) {
+				return null;
+			}
+		else
+			try {
+				return new User(r.getString("login"), r.getString("password"), r.getString("email"),
+						r.getString("nickname"));
+			} catch (InvalidUserException e) {
+				return null;
+			}
+	}
+}
