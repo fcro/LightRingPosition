c8f06d1c95755094f6ec114f3ddf8c4a900f3b75
mkiam <migan_karen@yahoo.fr>
2015-03-18 16:28:52 +0100

    petite modif au niveau du tchat

---
+++ server/src/main/java/fr/univ_lille1/iut/lightringposition/game/Plateau.java
@@ -0,0 +1,61 @@
+package fr.univ_lille1.iut.lightringposition.game;
+
+import java.util.Random;
+
+public class Plateau {
+	int val;
+	char[][]plateau;
+	int taille;
+	char valeurObstacles;
+	public Plateau(int taille, char valeurObstacles){
+		this.taille = taille;
+		this.valeurObstacles = valeurObstacles;
+		this.plateau= new char[taille][taille];
+
+	}
+
+	public void generation(){
+	
+		for(int i=0;i<taille; ++i){
+			for( int j=0; j<taille; ++j){
+				plateau[i][j]='.';
+
+			}
+		}
+		Random r= new Random();
+		val = r.nextInt(plateau.length*plateau.length*10/100)+plateau.length*plateau.length*10/100;
+		for(int i=0; i<=val;i++)
+			plateau[r.nextInt(plateau.length)][r.nextInt(plateau.length)]=valeurObstacles;
+
+
+
+	}
+	public String toString(){
+
+		    String aString;     
+		    aString = "";
+		    int column;
+		    int row;
+
+		    for (row = 0; row < plateau.length; row++) {
+		        for (column = 0; column < plateau[0].length; column++ ) {
+		        aString = aString + " " + plateau[row][column];
+		        }
+		    aString = aString + "\n";
+		    }
+
+		    return aString;
+		}
+	
+	public int getVal() {
+		return val;
+	}
+
+
+	public static void main(String[]args){
+		Plateau p= new Plateau(20,'Y');
+		p.generation();
+		System.out.println(p);
+		
+	}
+}
--- src/test/java/fr/univ_lille1/iut/lightringposition/test/TestPlateau.java
+++ src/test/java/fr/univ_lille1/iut/lightringposition/test/TestPlateau.java
@@ -1,7 +1,11 @@
 package fr.univ_lille1.iut.lightringposition.test;
 
+<<<<<<< Updated upstream:src/test/java/fr/univ_lille1/iut/lightringposition/test/TestPlateau.java
 import java.util.ArrayList;
 import java.util.List;
+=======
+import javax.validation.constraints.AssertTrue;
+>>>>>>> Stashed changes:server/src/test/java/fr/univ_lille1/iut/lightringposition/test/TestPlateau.java
 
 import org.junit.Test;
 
@@ -12,12 +16,17 @@ import static org.junit.Assert.*;
 public class TestPlateau {
 	@Test
 	public void test(){
+<<<<<<< Updated upstream:src/test/java/fr/univ_lille1/iut/lightringposition/test/TestPlateau.java
 		List<Joueur> liste = new ArrayList<Joueur>();
 		for(int i=0;i<10;i++) {
 			liste.add(new Joueur("toto"));
 		}
 		for(int i=0;i<10000;i++){
 			Plateau p = new Plateau(20,liste);
+=======
+		for(int i=0;i<1000000;i++){
+			Plateau p = new Plateau(20,'X');
+>>>>>>> Stashed changes:server/src/test/java/fr/univ_lille1/iut/lightringposition/test/TestPlateau.java
 			p.generation();
 			assertTrue(p.getVal() <= 80);
 			assertTrue(p.getVal() >=40);
